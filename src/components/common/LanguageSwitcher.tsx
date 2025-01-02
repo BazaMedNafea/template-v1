2a6604aa3ae8d346d8e02a8dd65d8d31808568f5
Mohamed <bazman.social@gmail.com>
2025-01-02 13:17:04 +0100

    oops

--- src/components/common/LanguageSwitcher.tsx
+++ src/components/common/LanguageSwitcher.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { LanguageContext } from "../contexts/LanguageContext";
+import { LanguageContext } from "../../contexts/LanguageContext";
 
 const LanguageSwitcher = () => {
   const { language, setLanguage } = useContext(LanguageContext);
